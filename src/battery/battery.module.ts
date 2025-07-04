1a8679d06d82e7562d49c3f7742bba0b2ae109ca
Evan <yrv.eg628@gmail.com>
2025-07-04 11:01:53 +0000

    add battery controller

--- src/battery/battery.module.ts
+++ src/battery/battery.module.ts
@@ -3,10 +3,11 @@ import { BatteryService } from './battery.service';
 import { ModbusModule } from '../modbus/modbus.module';
 import { BatteryBackupController } from './batteryBackup.controller';
 import { AcService } from './ac.service';
+import { BatteryController } from './battery.controller';
 
 @Module({
   imports: [ModbusModule],
-  controllers: [BatteryBackupController],
+  controllers: [BatteryBackupController, BatteryController],
   providers: [BatteryService, AcService],
   exports: [BatteryService, AcService],
 })
